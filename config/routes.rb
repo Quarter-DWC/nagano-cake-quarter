6cca5d18c7df86487764fbdc4394a7e1dd8946fe
yuto-kawahara <mashroom1384@gmail.com>
2021-10-16 06:43:24 +0000

    [Add]管理者と会員のDeviseコントローラーとモデルを追加
    管理者側と会員側のログイン・新規登録を行うため

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,55 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: 'admin/sessions'
+  }
+
+  namespace :admin do
+    root to: 'homes#top'
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :products, except: [:destroy] do
+      collection do
+        get 'search'
+      end
+    end
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
+
+
+  # 顧客用
+  devise_for :customers,skip: [:passwords,], controllers: {
+    registrations: 'customer/registrations',
+    sessions:      'customer/sessions'
+  }
+
+  root to: 'homes#top'
+  get '/about' => 'homes#about'
+  resources :products, only: [:index, :show] do
+    collection do
+      get 'search'
+    end
+  end
+  resources :cart_products, only: [:index, :create, :update, :destroy] do
+    collection do
+      delete 'destroy_all'
+    end
+  end
+  resources :orders, only: [:new, :create, :index, :show] do
+    collection do
+      post 'confirm'
+      get 'thanks'
+    end
+  end
+  resource :customers, only: [:edit, :update] do
+    collection do
+      get 'mypage' => 'customers#show'
+      get 'unsubscribe'
+      patch 'withdraw'
+    end
+  end
+  resources :deliveries, except: [:new, :show]
+
 end
