a1ad31b1fd28dd327e7a682420994f16e44442e1
konomi1 <89084813+konomi1@users.noreply.github.com>
2021-10-16 09:26:04 +0000

    [Fix]cart-produstsマイグレーションファイル修正

--- db/migrate/20211016074730_create_cart_products.rb
+++ db/migrate/20211016074730_create_cart_products.rb
@@ -1,7 +1,7 @@
 class CreateCartProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :cart_products do |t|
-      t.integer　:customer_id,   null: false, default: ""
+      t.integer  :customer_id,   null: false, default: ""
       t.integer  :product_id,    null: false, default: ""
       t.integer  :quantity,      null: false, default: ""
 
