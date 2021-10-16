d9fc55ca2f9689a75bb49ec623fa1dbda277482e
konomi1 <89084813+konomi1@users.noreply.github.com>
2021-10-16 07:52:15 +0000

    [Add]Cart_Productモデルのマイグレーションファイルを追加

---
+++ db/migrate/20211016074730_create_cart_products.rb
@@ -0,0 +1,11 @@
+class CreateCartProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cart_products do |t|
+      t.integer　:customer_id,   null: false, default: ""
+      t.integer  :product_id,    null: false, default: ""
+      t.integer  :quantity,      null: false, default: ""
+
+      t.timestamps
+    end
+  end
+end
