18a2907fabae5da5bdb1a93a9f502a7b78ed8297
yuto-kawahara <mashroom1384@gmail.com>
2021-10-17 02:05:44 +0000

    [Add]全モデルのテストデータを作成
    チームメンバーがデバッグ用に使用するテストデータを追加しました

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,79 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = Admin.new(
+  email: "test@test.com",
+  password: "tester123")
+
+admin.save!
+
+Product.create!(
+  genre_id: 1,
+  name: "ショートケーキ",
+  introduction: "美味しいケーキです",
+  price: 280,
+  image_id: "1",
+  sale_status: 0)
+
+Genre.create!(
+  [
+    {
+      name: "ケーキ"
+    },
+    {
+      name: "焼き菓子"
+    },
+    {
+      name: "キャンディ"
+    },
+    {
+      name: "プリン"
+    }
+  ])
+
+customer = Customer.new(
+  last_name: "山田",
+  first_name: "太郎",
+  kana_last_name: "ヤマダ",
+  kana_first_name: "タロウ",
+  email: "yamada@test.com",
+  phone_number: "00000000000",
+  postal_code: "0000000",
+  address: "東京都",
+  password: "test123",
+  is_valid: true
+  )
+
+customer.save!
+
+Order.create!(
+  customer_id: 1,
+  payment_method: 0,
+  fee: 800,
+  total_price: 1080,
+  postal_code: "0000000",
+  address: "東京都",
+  address_name: "山田太郎",
+  order_status: "0"
+  )
+
+OrderDetail.create!(
+  order_id: 1,
+  product_id: 1,
+  quantity: 1,
+  make_status: 0,
+  )
+
+CartProduct.create!(
+  customer_id: 1,
+  product_id: 1,
+  quantity: 1,
+  )
+
+Delivery.create!(
+  customer_id: 1,
+  postal_code: "1230000",
+  address: "大阪府",
+  address_name: "山田次郎"
+  )
