0107c329d1859ef62dcc8684becc217ca6f5be56
taku8a <takuya0505hsmt@gmail.com>
2021-10-18 14:38:26 +0000

    [Fix]orders詳細画面,コントローラーの調整
    自動更新機能を稼働させるため

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,15 +3,31 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    @order_details = OrderDetail.all
+    @order_details = @order.order_details
   end
-
+  # confirm_payment == 入金確認　make_status:1 == 制作待ち
   def update
+    order = Order.find(params[:id])
+    order_details = order.order_details
+    if order.update(order_params)
+      if order.order_status == "confirm_payment"
+         order_details.each { |order_detail|
+         order_detail.update(make_status:1)
+         }
+      end
+      redirect_to admin_order_path(order)
+    else
+      redirect_to request.referer
+    end
+
+
 
   end
 
-  def t
+  private
 
+  def order_params
+    params.require(:order).permit(:order_status)
   end
 
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -8,6 +8,11 @@ class Order < ApplicationRecord
     now_packing: 3,       # 発送準備中
     complete_send: 4      # 発送済み
   }
+  
+  enum payment_method: {
+    credit_card: 0,       #銀行振込
+    transfer: 1           #クレジットカード
+  }
 
   validates :postal_code, presence: true, length: { is: 7 }, numericality: {only_integer: true}
   validates :address, presence: true
