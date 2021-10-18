991c8843dd267c5dba6ad64942c59c28a0c4a03a
ichi-a00 <prenehru@gmail.com>
2021-10-18 06:32:42 +0000

    [Fix]authenticate_customer

--- app/controllers/customer/homes_controller.rb
+++ app/controllers/customer/homes_controller.rb
@@ -1,5 +1,5 @@
 class Customer::HomesController < ApplicationController
-  before_action :authenticate_user!, except: [:top, :about]
+  before_action :authenticate_customer!, except: [:top, :about]
   def top
     @products = Product.order(created_at: :desc).limit(5)
     @genres = Genre.includes(:products)
