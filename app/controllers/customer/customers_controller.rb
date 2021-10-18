5f7ed199fceba7c3bb0f4c2bd13b401b19d67f34
konomi1 <89084813+konomi1@users.noreply.github.com>
2021-10-18 10:51:32 +0000

    [Add]customer_controllerに退会処理追加
    退会ステータスをfalseに更新できるようupdateアクション追加

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -19,7 +19,13 @@ class Customer::CustomersController < ApplicationController
   end
 
   def withdraw
-    
+    if current_customer.update(is_valid: false)
+      redirect_to destroy_customer_session_path, notice: "退会しました。"
+    else
+      # ここは必要？必要ならunsubscribeにflash追加。
+      flash.naw[:alert] = "退会できませんでした"
+      render :unsubscribe
+    end
   end
 
   private
