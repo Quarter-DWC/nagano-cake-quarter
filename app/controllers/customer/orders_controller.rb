f5271a163ecf2f03f3dda75364cc13127395a323
konomi1 <89084813+konomi1@users.noreply.github.com>
2021-10-19 09:21:36 +0000

    [Add]ordersのコントローラーとビューファイル生成
    developブランチから新しいブランチを作成した際に伴った変更

---
+++ app/controllers/customer/orders_controller.rb
@@ -0,0 +1,21 @@
+class Customer::OrdersController < ApplicationController
+
+  def new
+  end
+
+  def confirm
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def thanks
+  end
+
+end
