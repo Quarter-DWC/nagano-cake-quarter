441f18626bb5ea921157ef7aba475d3678f77727
taku8a <takuya0505hsmt@gmail.com>
2021-10-19 00:26:21 +0000

    [Fix]order_detailsコントローラーへupdateアクション条件分岐追加
    updateが更新されなかった際の挙動を追加

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -4,19 +4,19 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order = @order_detail.order
-    @order_detail.update(order_detail_params)
+    if @order_detail.update(order_detail_params)
 
 
-    # order_status　2→制作中　3→発送準備中
+    # order_status:　2→制作中　3→発送準備中   make_status:   now_cooking→制作中　complete_cooking→制作完了
 
-    case @order_detail.make_status_i18n
-      when "制作中"
-        @order.update(order_status: 2)
-      when "制作完了"
-        if @order.order_details.all?{ |order_detail| order_detail.make_status_i18n == "制作完了" }
-           @order.update(order_status: 3)
-        end
-    end
+      case @order_detail.make_status
+        when "now_cooking"
+          @order.update(order_status: 2)
+        when "complete_cooking"
+          if @order.order_details.all?{ |order_detail| order_detail.make_status == "complete_cooking" }
+             @order.update(order_status: 3)
+          end
+      end
 
 
     # if @order_detail.make_status_i18n == "制作中"
@@ -27,7 +27,10 @@ class Admin::OrderDetailsController < ApplicationController
     #       @order_detail.order.save
     # end
 
-    redirect_to admin_order_path(@order_detail.order)
+      redirect_to admin_order_path(@order_detail.order)
+    else
+      redirect_to request.referer
+    end
   end
 
   private
