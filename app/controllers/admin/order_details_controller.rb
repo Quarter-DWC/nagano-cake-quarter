90cdcc090b9d31022468a11f7078247ce1c2f1f8
taku8a <takuya0505hsmt@gmail.com>
2021-10-19 07:03:22 +0000

    [Fix]修正版

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -7,25 +7,16 @@ class Admin::OrderDetailsController < ApplicationController
     if @order_detail.update(order_detail_params)
 
 
-    # order_status:　2→制作中　3→発送準備中   make_status:   now_cooking→制作中　complete_cooking→制作完了
+    # order_status:　now_cooking→制作中　now_packing→発送準備中   make_status:   now_cooking→制作中　complete_cooking→制作完了
 
       case @order_detail.make_status
         when "now_cooking"
-          @order.update(order_status: 2)
+          @order.update(order_status: "now_cooking")
         when "complete_cooking"
           if @order.order_details.all?{ |order_detail| order_detail.make_status == "complete_cooking" }
-             @order.update(order_status: 3)
+             @order.update(order_status: "now_packing")
           end
       end
-
-
-    # if @order_detail.make_status_i18n == "制作中"
-    #   @order_detail.order.order_status_i18n = "制作中"
-    #   @order_detail.order.save
-    # elsif @order_detail.make_status_i18n == "制作完了"
-    #       @order_detail.order.order_status_i18n = "発送準備中"
-    #       @order_detail.order.save
-    # end
 
       redirect_to admin_order_path(@order_detail.order)
     else
