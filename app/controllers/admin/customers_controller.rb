f1b609ec8a9f894b4cc57b6216601ed71c5e90ad
taku8a <takuya0505hsmt@gmail.com>
2021-10-20 01:41:20 +0000

    [start][Add]会員詳細・一覧・編集機能を実装

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,44 @@
+class Admin::CustomersController < ApplicationController
+  # before_action :authenticate_admin!
+  before_action :setup_customer, except: [:index]
+
+  def index
+    @customers = Customer.page(params[:page]).per(10).order(id: "ASC")
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @customer.update!(customer_params)
+      redirect_to admin_customer_path(@customer)
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(
+      :id,
+      :last_name,
+      :first_name,
+      :kana_last_name,
+      :kana_first_name,
+      :email,
+      :phone_number,
+      :postal_code,
+      :address,
+      :is_valid,
+      )
+  end
+
+  def setup_customer
+    @customer = Customer.find(params[:id])
+  end
+
+end
