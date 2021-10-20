e7a8430818a6de413c7645d4883cb68cad5e0d99
taku8a <takuya0505hsmt@gmail.com>
2021-10-20 07:03:32 +0000

    [Add]order_detailsコントローラーを追加
    制作ステータスを変更するため

---
+++ app/controllers/admin/order_details_controller.rb
@@ -0,0 +1,32 @@
+class Admin::OrderDetailsController < ApplicationController
+  before_action :authenticate_admin!
+
+  def update
+    @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    if @order_detail.update(order_detail_params)
+
+
+    # order_status:　now_cooking→制作中　now_packing→発送準備中   make_status:   now_cooking→制作中　complete_cooking→制作完了
+
+      case @order_detail.make_status
+        when "now_cooking"
+          @order.update_attributes(order_status: "now_cooking")
+        when "complete_cooking"
+          if @order.order_details.all?{ |order_detail| order_detail.make_status == "complete_cooking" }
+             @order.update_attributes(order_status: "now_packing")
+          end
+      end
+
+      redirect_to admin_order_path(@order_detail.order)
+    else
+      redirect_to request.referer
+    end
+  end
+
+  private
+
+  def order_detail_params
+    params.require(:order_detail).permit(:make_status)
+  end
+end
