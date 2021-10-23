87a328968e63e1f2a2ca83b69363771633ed1b1f
erika <prenehru@gmail.com>
2021-10-23 00:58:52 +0000

    [Fix]productモデルにバリデーション

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,6 +13,7 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :introduction, presence: true
   validates :price, presence: true, numericality: {only_integer: true}
+  validates :genre_id, presence: true, numericality: {only_integer: true}
 
   #税込価格=税抜価格*1.1、小数点以下切り捨て
   def tax_included_price
